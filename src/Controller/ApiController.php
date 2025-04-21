fbbb3ebf554ca0fc7a5014a8bd25e71bf919261c
alx <world.zay@gmail.com>
2025-04-21 15:14:21 +0300

    feat: Update ApiTest controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,18 +2,25 @@
 
 namespace Sys\Controller;
 
+use Sys\Contract\UserInterface;
 use Sys\Middleware\CORSMiddleware;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class ApiController extends BaseController
 {
     protected array $headers;
+    protected ?UserInterface $user;
 
     public function __construct(protected CORSMiddleware $cors){}
 
-    protected function _before()
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $contract = config('api_contracts', $this->request->getUri()->getPath());
-        $this->headers = $this->cors->getHeaders($this->request, $contract);
+        $contract = config('api_contracts', $request->getUri()->getPath());
+        $this->headers = $this->cors->getHeaders($request, $contract);
+        $this->user = $request->getAttribute('user');
+
+        return parent::handle($request);
     }
 
 }

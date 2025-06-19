e9e7cc922e9a444d5eea91d60fd4640b1090c565
alx <world.zay@gmail.com>
2025-06-19 17:52:01 +0300

    Update src/Config/container.php

--- src/Config/container.php
+++ src/Config/container.php
@@ -23,17 +23,14 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Psr\Http\Server\RequestHandlerInterface;
 use Sys\Contract\UserInterface;
-use Sys\PostProcess\PostProcessInterface;
-use Sys\PostProcess\PostProcess;
 use Sys\Template\TemplateFactory;
 use Sys\Template\TemplateInterface;
 use Sys\I18n\Model\File as I18nModelFile;
 use Sys\I18n\Model\I18nModelInterface;
 use Sys\Pipeline\Pipeline;
 use Sys\Pipeline\PipelineInterface;
-use Sys\Profiler\Model\Mysql;
-use Sys\Profiler\Model\ProfilerModelInterface;
-use Sys\Profiler\Profiler;
+use Sys\Pipeline\PostProcess;
+use Sys\Pipeline\PostProcessInterface;
 
 return [
     ServerRequestInterface::class => fn() => (new ServerRequestCreator())->create(),
@@ -59,7 +56,7 @@ return [
         ExceptionResponseFactory $response_factory) 
         => new WhoopsAdapter($request, $logger, $emitter, $response_factory),
     
-    PostProcessInterface::class => fn() => new PostProcess(),
+    PostProcessInterface::class => fn(ContainerInterface $c) => new PostProcess($c),
     IQueryBuilderHandler::class => fn() => (new Connection('mysql', config('database', 'connect.mysql')))->getQueryBuilder(),
     
     SessionInterface::class => function (ContainerInterface $c) {
@@ -79,8 +76,4 @@ return [
     I18nModelInterface::class => fn() => new I18nModelFile(findPaths('i18n')),
 
     UserInterface::class => User::class,
-    
-    // ProfilerModelInterface::class => fn(ContainerInterface $c) => $c->get(Mysql::class),
-    // Profiler::class => fn(ContainerInterface $c)
-    //     => new Profiler($c->get(ProfilerModelInterface::class), $c->get(RouteCollectionInterface::class)),
 ];

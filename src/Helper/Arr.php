10003e7d2430d85ef88712d7efdc641bf6d3dadb
alx <world.zay@gmail.com>
2025-12-09 12:55:14 +0300

    #42 Create Helper\Arr and Facade\Arr

---
+++ src/Helper/Arr.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys\Helper;
+
+class Arr
+{
+    public function flatten(array $array, string $separator = '.', string $prefix = ''): array
+    {
+        $result = [];
+
+        foreach ($array as $key => $value) {
+            $newKey = $prefix === '' ? $key : $prefix . '.' . $key;
+            if (is_array($value)) {
+                if (array_is_list($value)) {
+                    foreach ($value as $item) {
+                        if (is_array($item)) {
+                            $result = array_merge_recursive($result, $this->flatten($item, $separator, $newKey));
+                        } else {
+                            $result[$newKey][] = $item;
+                        }
+                    }
+                } else {
+                    $result = array_merge_recursive($result, $this->flatten($value, $separator, $newKey));
+                }
+            } else {
+                $result[$newKey] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    public function unflatten(array $flattenedArray, string $separator = '.'): array
+    {
+        $result = [];
+
+        foreach ($flattenedArray as $key => $value) {
+            $parts = explode($separator, $key);
+            $current = &$result; // Use a reference to traverse the array
+
+            foreach ($parts as $part) {
+                if (!isset($current[$part]) || !is_array($current[$part])) {
+                    $current[$part] = [];
+                }
+                $current = &$current[$part];
+            }
+            $current = $value; // Assign the value to the deepest level
+        }
+
+        return $result;
+    }
+
+    public function preparePost($array)
+    {
+        array_walk_recursive($array, function (&$value) {
+            if (is_string($value)) {
+                $value = trim($value);
+            }
+
+            $value = match (filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE)) {
+                false => 0,
+                true => 1,
+                default => $value,
+            };
+
+            if (is_numeric($value) && is_string($value)) {
+                if (str_contains($value, '.')) {
+                    settype($value, 'float');
+                } else {
+                    settype($value, 'integer');
+                }
+            }
+        });
+
+        return $array;
+    }
+}
---
+++ src/Helper/Facade/Arr.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys\Helper\Facade;
+
+class Arr extends FacadeAbstract {}

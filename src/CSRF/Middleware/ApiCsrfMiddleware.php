5817f47d47842f49bd61e832c6d7281b604e55bd
alx <world.zay@gmail.com>
2026-03-24 12:10:38 +0300

    #59 Create ApiCsrfMiddleware

---
+++ src/CSRF/Middleware/ApiCsrfMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys\CSRF\Middleware;
+
+use Sys\CSRF\Facade\Csrf;
+use HttpSoft\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ApiCsrfMiddleware implements MiddlewareInterface
+{
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+        if (in_array($request->getMethod(), ['GET', 'HEAD', 'OPTIONS'])) {
+            return $handler->handle($request);
+        }
+        
+        $user = $request->getAttribute('user');
+        $token = $request->getHeaderLine('X-CSRF');
+        $valid = Csrf::validate($token, $user?->id);
+
+        return $valid ? $handler->handle($request) : new JsonResponse('Token not match', 400);
+    }
+}

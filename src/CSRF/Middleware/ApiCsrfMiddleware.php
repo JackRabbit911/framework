a42f888f9c65304f0aca0ec0560889995ef156b0
alx <world.zay@gmail.com>
2026-03-26 15:29:59 +0300

    #59 Add getData and removeCsrf functions

--- src/CSRF/Middleware/ApiCsrfMiddleware.php
+++ src/CSRF/Middleware/ApiCsrfMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sys\CSRF\Middleware;
 
 use Sys\CSRF\Facade\Csrf;
+use HttpSoft\Message\StreamFactory;
 use HttpSoft\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -13,6 +14,17 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class ApiCsrfMiddleware implements MiddlewareInterface
 {
+    public const FORM = 'form';
+    public const BODY = 'body';
+    public const QUERY = 'query';
+
+    private string $source;
+
+    public function __construct(string $source = self::BODY)
+    {
+        $this->source = $source;
+    }
+
     public function process(
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
@@ -21,22 +33,39 @@ class ApiCsrfMiddleware implements MiddlewareInterface
             return $handler->handle($request);
         }
 
-        $data = $request->getBody()->getContents();
-
-        if (empty($data)) {
-            $data = $request->getParsedBody();
-        }
-
-        if (is_string($data)) {
-            $data = json_decode($data, true) ?? [];
-        }
-
-        $data += $request->getQueryParams();
-        
+        $data = $this->getData($request);
         $user = $request->getAttribute('user');
         $token = $data['_csrf'] ?? $request->getHeaderLine('X-CSRF') ?? '';
         $valid = Csrf::validate($token, $user?->id);
 
-        return $valid ? $handler->handle($request) : new JsonResponse('Token not match', 400);
+        return $valid
+            ? $handler->handle($this->removeCsrf($request, $data))
+            : new JsonResponse('Token not match', 400);
+    }
+
+    private function getData(ServerRequestInterface $request)
+    {
+        return match ($this->source) {
+            self::BODY => json_decode($request->getBody()->getContents(), true) ?? [],
+            self::FORM => $request->getParsedBody(),
+            self::QUERY => $request->getQueryParams(),
+        };
+    }
+
+    private function removeCsrf(ServerRequestInterface  $request, $data)
+    {
+        unset($data['_csrf']);
+
+        switch ($this->source) {
+            case self::BODY:
+                $factory = new StreamFactory();
+                $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+                $stream = $factory->createStream($json);
+                return $request->withBody($stream);
+            case self::FORM:
+                return $request->withParsedBody($data);
+            case self::QUERY:
+                return $request->withQueryParams($data);
+        }
     }
 }

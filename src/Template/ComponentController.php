025132bdc82a54d5ca68dd3fe80f10d794380e75
alx <world.zay@gmail.com>
2026-05-18 16:47:02 +0300

    #85 Create ComponentController abstract class

---
+++ src/Template/ComponentController.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sys\Template;
+
+use Az\Route\Route;
+use HttpSoft\Response\EmptyResponse;
+use HttpSoft\Response\HtmlResponse;
+use HttpSoft\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Sys\Contract\UserInterface;
+use Sys\Controller\InvokeTrait;
+
+abstract class ComponentController extends Component implements RequestHandlerInterface, CmpInterface
+{
+    use InvokeTrait;
+
+    protected ServerRequestInterface $request;
+    protected ?UserInterface $user;
+    protected string $view;
+    protected array $data = [];
+    protected ?string $js = null;
+    // private array $parameters;
+    private TemplateInterface $tpl;
+    private App $app;
+
+    public function __construct()
+    {
+        $this->tpl = container()->get(TemplateInterface::class);
+        $this->app = container()->get(App::class);
+        $this->tpl->addGlobal('app', $this->app);
+        $this->js($this->js);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->request = $request;
+        $this->user = $request->getAttribute('user');
+        $this->app->add('user', $this->user);
+        
+        $route = $request->getAttribute(Route::class);
+        // $this->parameters = $route->getParameters();
+        [$handler, $action] = $route->getHandler();
+
+        return $this->call($action, $route->getParameters());
+    }
+
+    public function js(?string $script = null)
+    {
+        if ($script) {
+            $this->app->js($script);
+        }
+
+        return $this;
+    }
+
+    protected function html(): ResponseInterface
+    {
+        return new HtmlResponse($this->render());
+    }
+
+    protected function json(): ResponseInterface
+    {
+        return new JsonResponse($this->data);
+    }
+
+    protected function empty(): ResponseInterface
+    {
+        return new EmptyResponse();
+    }
+}
